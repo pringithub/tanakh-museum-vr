8df9185083be53d7b3921cda686a9ec4dee0ff61
Phillip Ring <pring5@ford.com>
2020-09-28 14:56:29 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tanakh-museum-vr
+A VR Museum to Showcase Art from the Tanakh - built using a-frame
---
+++ js/website_links.js
@@ -0,0 +1,137 @@
+
+// website_links.js
+// Description: this file is meant to house external links 
+
+// Torah
+
+var genesis_links = ["/art/large/1096.html", "/art/large/70.html", "/art/large/664.html", "/art/large/307.html", "/art/large/663.html", "/art/large/170.html", "/art/large/560.html", "/art/large/66.html", "/art/large/273.html", "/art/large/1082.html", "/art/large/850.html", "/art/large/561.html", "/art/large/67.html", "/art/large/849.html", "/art/large/203.html", "/art/large/306.html", "/art/large/456.html", "/art/large/506.html", "/art/large/917.html", "/art/large/293.html", "/art/large/517.html", "/art/large/792.html", "/art/large/68.html", "/art/large/515.html", "/art/large/576.html", "/art/large/295.html", "/art/large/518.html", "/art/large/851.html", "/art/large/291.html", "/art/large/516.html", "/art/large/784.html", "/art/large/358.html", "/art/large/575.html", "/art/large/282.html", "/art/large/677.html", "/art/large/482.html", "/art/large/69.html", "/art/large/852.html", "/art/large/292.html", "/art/large/919.html", "/art/large/562.html", "/art/large/599.html", "/art/large/764.html", "/art/large/81.html", "/art/large/853.html", "/art/large/624.html", "/art/large/62.html", "/art/large/918.html", "/art/large/271.html", "/art/large/272.html", "/art/large/167.html", "/art/large/1083.html", "/art/large/567.html", "/art/large/60.html", "/art/large/71.html", "/art/large/997.html", "/art/large/202.html", "/art/large/996.html", "/art/large/998.html", "/art/large/144.html", "/art/large/878.html", "/art/large/665.html", "/art/large/145.html", "/art/large/153.html", "/art/large/122.html", "/art/large/622.html", "/art/large/620.html", "/art/large/296.html", "/art/large/877.html", "/art/large/621.html", "/art/large/492.html", "/art/large/671.html", "/art/large/529.html", "/art/large/477.html", "/art/large/186.html", "/art/large/826.html", "/art/large/471.html", "/art/large/958.html", "/art/large/84.html", "/art/large/541.html", "/art/large/520.html", "/art/large/929.html", "/art/large/804.html", "/art/large/1019.html", "/art/large/806.html", "/art/large/63.html", "/art/large/1068.html", "/art/large/468.html", "/art/large/453.html", "/art/large/1028.html", "/art/large/82.html", "/art/large/86.html", "/art/large/472.html", "/art/large/1062.html", "/art/large/274.html", "/art/large/670.html", "/art/large/441.html", "/art/large/2.html", "/art/large/910.html", "/art/large/474.html","/art/large/765.html", "/art/large/803.html", "/art/large/1017.html", "/art/large/108.html", "/art/large/549.html", "/art/large/85.html", "/art/large/980.html", "/art/large/364.html", "/art/large/979.html", "/art/large/568.html", "/art/large/83.html", "/art/large/1023.html", "/art/large/470.html", "/art/large/459.html", "/art/large/350.html", "/art/large/88.html", "/art/large/1006.html", "/art/large/473.html", "/art/large/715.html", "/art/large/603.html", "/art/large/89.html", "/art/large/604.html", "/art/large/600.html", "/art/large/87.html", "/art/large/1103.html", "/art/large/563.html", "/art/large/564.html", "/art/large/742.html", "/art/large/547.html", "/art/large/946.html", "/art/large/554.html", "/art/large/555.html", "/art/large/809.html", "/art/large/361.html", "/art/large/838.html", "/art/large/960.html", "/art/large/360.html", "/art/large/137.html", "/art/large/261.html", "/art/large/935.html", "/art/large/1040.html", "/art/large/1037.html", "/art/large/711.html", "/art/large/138.html", "/art/large/699.html", "/art/large/698.html", "/art/large/421.html"]
+
+var exodus_links = ["/art/large/750.html", "/art/large/109.html", "/art/large/410.html", "/art/large/662.html", "/art/large/112.html", "/art/large/379.html", "/art/large/827.html", "/art/large/1042.html", "/art/large/587.html", "/art/large/142.html", "/art/large/710.html", "/art/large/533.html", "/art/large/496.html", "/art/large/588.html", "/art/large/535.html", "/art/large/141.html", "/art/large/476.html", "/art/large/1104.html", "/art/large/110.html", "/art/large/839.html", "/art/large/140.html", "/art/large/177.html", "/art/large/61.html", "/art/large/126.html", "/art/large/269.html", "/art/large/1043.html", "/art/large/382.html", "/art/large/661.html", "/art/large/113.html", "/art/large/971.html"]
+
+var leviticus_links = ["/art/large/1000.html"]
+
+var numbers_links = ["/art/large/673.html", "/art/large/904.html", "/art/large/972.html", "/art/large/716.html", "/art/large/345.html", "/art/large/166.html", "/art/large/143.html", "/art/large/407.html", "/art/large/713.html", "/art/large/199.html"]
+
+var deuteronomy_links = ["/art/large/511.html"]
+
+
+// Nevi'im
+
+var joshua_links = ["/art/large/961.html", "/art/large/298.html", "/art/large/869.html", "/art/large/139.html", "/art/large/821.html", "/art/large/868.html"]
+
+var judges_links = ["/art/large/934.html", "/art/large/727.html", "/art/large/893.html", "/art/large/213.html", "/art/large/802.html", "/art/large/1014.html", "/art/large/297.html", "/art/large/565.html", "/art/large/618.html", "/art/large/789.html", "/art/large/303.html", "/art/large/149.html", "/art/large/411.html", "/art/large/200.html", "/art/large/920.html", "/art/large/127.html", "/art/large/1093.html", "/art/large/577.html", "/art/large/275.html", "/art/large/442.html", "/art/large/1033.html", "/art/large/1003.html"]
+
+var shmuel1_links = ["/art/large/1090.html", "/art/large/338.html", "/art/large/114.html", "/art/large/1072.html", "/art/large/490.html", "/art/large/363.html", "/art/large/669.html", "/art/large/435.html", "/art/large/1.html", "/art/large/900.html", "/art/large/724.html", "/art/large/378.html", "/art/large/700.html", "/art/large/115.html", "/art/large/725.html", "/art/large/648.html", "/art/large/728.html", "/art/large/840.html", "/art/large/945.html", "/art/large/300.html"]
+
+var shmuel2_links = ["/art/large/718.html", "/art/large/717.html", "/art/large/1069.html", "/art/large/124.html", "/art/large/679.html", "/art/large/412.html", "/art/large/172.html", "/art/large/1045.html", "/art/large/860.html", "/art/large/194.html", "/art/large/1059.html", "/art/large/538.html", "/art/large/1002.html"]
+
+var kings1_links = ["/art/large/542.html", "/art/large/173.html", "/art/large/1024.html", "/art/large/147.html", "/art/large/922.html", "/art/large/475.html", "/art/large/828.html", "/art/large/800.html", "/art/large/933.html", "/art/large/1036.html", "/art/large/730.html", "/art/large/712.html", "/art/large/899.html", "/art/large/123.html", "/art/large/1018.html", "/art/large/184.html", "/art/large/1041.html"]
+
+var kings2_links = ["/art/large/201.html", "/art/large/762.html", "/art/large/798.html", "/art/large/1035.html", "/art/large/672.html"]
+
+var isaiah_links = ["/art/large/77.html", "/art/large/214.html", "/art/large/469.html", "/art/large/559.html", "/art/large/56.html", "/art/large/1012.html", "/art/large/666.html", "/art/large/256.html", "/art/large/941.html", "/art/large/912.html", "/art/large/558.html", "/art/large/846.html"]
+
+var jeremiah_links = ["/art/large/74.html", "/art/large/1027.html", "/art/large/215.html", "/art/large/129.html"]
+
+var ezekiel_links = ["/art/large/76.html", "/art/large/182.html", "/art/large/216.html", "/art/large/198.html"]
+
+var hosea_links     = ["/art/large/217.html"]
+var joel_links      = ["/art/large/72.html", "/art/large/218.html"]
+var amos_links      = ["/art/large/210.html", "/art/large/219.html"]
+var obadiah_links   = ["/art/large/220.html"]
+var jonah_links     = ["/art/large/73.html", "/art/large/197.html", "/art/large/330.html", "/art/large/810.html", "/art/large/221.html", "/art/large/786.html"]
+var micah_links     = ["/art/large/196.html", "/art/large/222.html"]
+var nahum_links     = ["/art/large/223.html"]
+var habakkuk_links  = ["/art/large/1026.html"]
+var zephaniah_links = ["/art/large/224.html"]
+var haggai_links    = ["/art/large/225.html"]
+var zechariah_links = ["/art/large/78.html", "/art/large/226.html", "/art/large/692.html"]
+var malachi_links   = ["/art/large/152.html", "/art/large/227.html"]
+var minor_prophets_links = [hosea_links, joel_links, amos_links, obadiah_links, jonah_links, micah_links, nahum_links, habakkuk_links, zephaniah_links, haggai_links, zechariah_links, malachi_links]
+
+
+// Ketuvim
+
+var psalms_links = ["/art/large/304.html"]
+
+var proverbs_links = []
+
+var job_links = ["/art/large/627.html", "/art/large/630.html", "/art/large/629.html", "/art/large/631.html", "/art/large/632.html", "/art/large/820.html", "/art/large/135.html", "/art/large/807.html", "/art/large/808.html", "/art/large/633.html", "/art/large/729.html", "/art/large/634.html", "/art/large/136.html", "/art/large/635.html", "/art/large/637.html", "/art/large/636.html", "/art/large/1011.html", "/art/large/825.html", "/art/large/628.html", "/art/large/638.html", "/art/large/642.html", "/art/large/639.html", "/art/large/626.html", "/art/large/640.html", "/art/large/641.html", "/art/large/643.html", "/art/large/644.html", "/art/large/645.html", "/art/large/647.html", "/art/large/646.html"]
+
+var song_of_songs_links = []
+var ruth_links = ["/art/large/368.html", "/art/large/212.html", "/art/large/132.html", "/art/large/502.html", "/art/large/676.html", "/art/large/987.html"]
+var lamentations_links = []
+var ecclesiastes_links = []
+var esther_links = ["/art/large/332.html", "/art/large/91.html", "/art/large/335.html", "/art/large/697.html", "/art/large/92.html", "/art/large/530.html", "/art/large/336.html", "/art/large/337.html", "/art/large/93.html", "/art/large/334.html", "/art/large/534.html", "/art/large/818.html", "/art/large/94.html", "/art/large/333.html", "/art/large/539.html", "/art/large/174.html"]
+var megillah_links = [song_of_songs_links, ruth_links, lamentations_links, ecclesiastes_links, esther_links]
+
+
+var daniel_links = ["/art/large/75.html", "/art/large/339.html", "/art/large/623.html", "/art/large/95.html", "/art/large/96.html", "/art/large/133.html", "/art/large/97.html"]
+
+var ezra_links = []
+
+var nehemiah_links = ["/art/large/1031.html"]
+
+var chronicles1_links = ["/art/large/674.html", "/art/large/299.html"]
+
+var chronicles2_links = ["/art/large/1020.html", "/art/large/148.html", "/art/large/675.html", "/art/large/211.html"]
+
+
+var book_links = [genesis_links, exodus_links, leviticus_links, numbers_links, deuteronomy_links, 
+			joshua_links, judges_links, shmuel1_links, shmuel2_links, kings1_links, kings2_links, 
+			isaiah_links, jeremiah_links, ezekiel_links, minor_prophets_links, 
+			psalms_links, proverbs_links, job_links, megillah_links, 
+			daniel_links, ezra_links, nehemiah_links, chronicles1_links, chronicles2_links]
+
+var book_names = ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy',
+			'Joshua', 'Judges', 'Samuel 1', 'Samuel 2', 'Kings 1', 'Kings 2', 
+			'Isaiah', 'Jeremiah', 'Ezekiel', 'Minor Prophets', 
+			'Psalms', 'Proverbs', 'Job', 'Megillah', 
+			'Daniel', 'Ezra', 'Nehemiah', 'Chronicles 1', 'Chronicles 2']
+
+
+// create link vector
+//var website_domain = 'https://www.artbible.info'
+//var websites = [];
+//for (i=0;i<genesis_links.length;i++) {
+//	websites.push(website_domain+genesis_links[i])
+//}
+
+// get list of art in biblically chronoligcal ordrer
+// not working due to clicking weirdness, annoying ..
+/*function getBooks() {
+	var art_list = []
+	var website = "https://www.artbible.info/art/books/"
+	$.ajax({ url: website, success: function(text) {
+			// Genesis, Exodus, etc
+			var book_num = 0
+			var book_href = $(text).find('.kolommen').children()[0].children[1].children[book_num]
+			book_href.children[0].click() // need to click to load options
+			var book_artpieces = book_href.children[2]
+
+			art_list = []
+			for (i=0;i<book_href.children.length;i++) {
+				art_link = book_href.children[i].children[0]
+				art_link = $(art_link).attr('href')
+				art_list.push(art_link)
+			}
+			console.log(art_list)
+		},
+		async:false
+	});
+}
+getBooks()
+*/
+
+
+/* click on the element to open div, then run this, replacing booknum with actual number
+booknum=#
+a = document.getElementById('deLijst'+booknum).children[0]
+l=[]			
+for (i=0;i<a.children.length;i++) {
+	art_link = a.children[i].children[0]
+	art_link = $(art_link).attr('href')
+	l.push(art_link)
+}
+console.log(l)
+*/
